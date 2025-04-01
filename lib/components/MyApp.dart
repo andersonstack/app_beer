833cd27846759c87fc926d0cc24528f2dd1d4acb
andersonstack <andersong.pereiracruz@gmail.com>
2025-04-01 19:54:34 -0300

    feat: componetizando o widget principal

---
+++ lib/components/MyApp.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyApp extends StatelessWidget {
+  final List<Map<String, dynamic>> cervejas;
+  final List<String> categorias;
+
+  const MyApp({super.key, required this.cervejas, required this.categorias});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFFF2F2F2),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          surface: Color(0xFFA62508),
+          primary: Color(0xFFF2A20C),
+          secondary: Color(0xFFF2A20C),
+        ),
+        textTheme: TextTheme(
+          bodyLarge: GoogleFonts.poppins(
+            color: Theme.of(context).colorScheme.surface,
+            fontSize: 25,
+            fontWeight: FontWeight.w500,
+          ),
+          bodyMedium: GoogleFonts.poppins(
+            color: Color(0xFFA62508),
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+          bodySmall: GoogleFonts.poppins(
+            color: Colors.black,
+            fontSize: 15,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,25 @@
+import "package:flutter/material.dart";
+import 'components/MyApp.dart';
 
+void main() {
+  List<Map<String, dynamic>> cervejas = [
+    {"name": "La Fin Du Monde", "style": "Bock", "ibu": "65"},
+    {"name": "Sapporo Premium", "style": "Sour Ale", "ibu": "54"},
+    {"name": "Duvel", "style": "Pilsner", "ibu": "82"},
+    {"name": "Carlsberg", "style": "Lager", "ibu": "50"},
+    {"name": "Stella Artois", "style": "Lager", "ibu": "50"},
+    {"name": "Heineken", "style": "Lager", "ibu": "50"},
+    {"name": "Corona", "style": "Lager", "ibu": "50"},
+    {"name": "Budweiser", "style": "Lager", "ibu": "50"},
+    {"name": "Amstel", "style": "Lager", "ibu": "50"},
+    {"name": "Carlsberg", "style": "Lager", "ibu": "50"},
+    {"name": "Sapporo Premium", "style": "Lager", "ibu": "50"},
+    {"name": "Stella Artois", "style": "Lager", "ibu": "50"},
+    {"name": "Corona", "style": "Lager", "ibu": "50"},
+  ];
+  List<String> categorias = ["name", "style", "ibu"];
+
+  MyApp app = MyApp(cervejas: cervejas, categorias: categorias);
+
+  runApp(app);
+}
