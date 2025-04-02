6840308c1cc704f27020063f9f481e1b97e269f0
andersonstack <andersong.pereiracruz@gmail.com>
2025-04-02 12:49:04 -0300

    fix: ajuste de uso incorreto em ParentDaraWidget

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -5,10 +5,8 @@ import '../controller/image_controller.dart';
 class HomeView extends StatelessWidget {
   final List<Map<String, dynamic>> cervejas;
   final List<String> categorias;
-  // ignore: prefer_typing_uninitialized_variables
-  final screenWidth;
-  // ignore: prefer_typing_uninitialized_variables
-  final screenHeight;
+  final double screenWidth;
+  final double screenHeight;
 
   const HomeView({
     super.key,
@@ -25,49 +23,39 @@ class HomeView extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.surface),
-      drawer: Drawer(),
-      body: Expanded(
-        child: Container(
-          width: double.infinity,
-          color: Theme.of(context).colorScheme.surfaceContainer,
-          child: Column(
-            children: [
-              GetX<ImageController>(
-                builder:
-                    (controller) => Positioned(
-                      top: controller.topPosition.value,
-                      left:
-                          MediaQuery.of(context).size.width / 2 -
-                          controller.imageSize.value / 2,
-                      child: AnimatedOpacity(
-                        opacity: controller.opacity.value,
-                        duration: Duration(seconds: 2),
-                        child: AnimatedContainer(
-                          duration: Duration(seconds: 2),
-                          width: controller.imageSize.value,
-                          height: controller.imageSize.value,
-                          child: Image.asset(
-                            "assets/beer.png",
-                            width: controller.imageSize.value,
-                            height: controller.imageSize.value,
-                          ),
-                        ),
-                      ),
+      drawer: const Drawer(),
+      body: Container(
+        width: double.infinity,
+        color: Theme.of(context).colorScheme.surfaceContainer,
+        child: Column(
+          children: [
+            GetX<ImageController>(
+              builder:
+                  (controller) => AnimatedContainer(
+                    duration: const Duration(seconds: 1),
+                    curve: Curves.linear,
+                    width: controller.imageSize.value,
+                    height: controller.imageSize.value,
+                    child: Image.asset(
+                      "assets/beer.png",
+                      width: controller.imageSize.value,
+                      height: controller.imageSize.value,
+                      fit: BoxFit.contain,
                     ),
+                  ),
+            ),
+            Text(
+              "Descubra, avalie e registre suas cervejas favoritas em um só lugar!",
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            TextButton(
+              onPressed: () => {},
+              child: Text(
+                "Visualizar cervejas",
+                style: Theme.of(context).textTheme.bodyLarge,
               ),
-              Text(
-                "Descubra, avalie e registre suas cervejas favoritas em um só lugar! ",
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-              TextButton(
-                onPressed: () => {},
-                child: Text(
-                  "Visualizar cervejas",
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
