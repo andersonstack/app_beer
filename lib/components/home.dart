e402b0d387b19de6ee0a9e8c86704f9d482a54b2
andersonstack <andersong.pereiracruz@gmail.com>
2025-04-02 08:39:44 -0300

    animate: adicionando animação

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -1,4 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ImageController extends GetxController {
+  var opacity = 0.0.obs;
+  var imageSize = 300.0.obs;
+  var topPosition = 250.0.obs;
+
+  void animate() {
+    Future.delayed(Duration(milliseconds: 500), () {
+      opacity.value = 1.0;
+      Future.delayed(Duration(seconds: 1), () {
+        imageSize.value = 100.0;
+        topPosition.value = 50.0;
+      });
+    });
+  }
+}
 
 class HomeView extends StatelessWidget {
   final List<Map<String, dynamic>> cervejas;
@@ -7,6 +24,9 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(ImageController());
+    controller.animate();
+
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.red),
       drawer: Drawer(),
@@ -16,7 +36,24 @@ class HomeView extends StatelessWidget {
           color: Colors.blue,
           child: Column(
             children: [
-              Image.asset("assets/beer.png"),
+              Obx(
+                () => Positioned(
+                  top: controller.topPosition.value,
+                  left:
+                      MediaQuery.of(context).size.width / 2 -
+                      controller.imageSize.value / 2,
+                  child: AnimatedOpacity(
+                    opacity: controller.opacity.value,
+                    duration: Duration(seconds: 2),
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 2),
+                      width: controller.imageSize.value,
+                      height: controller.imageSize.value,
+                      child: Image.asset("assets/beer.png"),
+                    ),
+                  ),
+                ),
+              ),
               Text(
                 "Descubra, avalie e registre suas cervejas favoritas em um só lugar! ",
               ),
