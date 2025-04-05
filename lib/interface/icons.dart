965cf4345acca4dbe1bd9816b63b6d5b6568c783
andersonstack <andersong.pereiracruz@gmail.com>
2025-04-05 16:20:22 -0300

    feat: add novo ícone

--- lib/interface/icons.dart
+++ lib/interface/icons.dart
@@ -9,6 +9,8 @@ class AppIcons {
         return Icon(Icons.filter_list_alt);
       case 'SEARCH':
         return Icon(Icons.manage_search);
+      case 'LIST':
+        return Icon(Icons.format_list_bulleted_rounded);
       default:
         return Icon(Icons.error); // Fallback para ícone de erro
     }
