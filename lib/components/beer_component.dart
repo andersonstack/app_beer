1da0df16b176a9e0e0cd40800cf09244a94e4f20
andersonstack <andersong.pereiracruz@gmail.com>
2025-04-09 14:13:12 -0300

    style: mostrando detalhes da cerveja

--- lib/components/beer_component.dart
+++ lib/components/beer_component.dart
@@ -1,4 +1,6 @@
+import 'package:app_beer/controller/app_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Beer extends StatelessWidget {
   final String id;
@@ -7,6 +9,17 @@ class Beer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("")));
+    final AppController controller = Get.find();
+    final beer = controller.cervejas[id]!;
+
+    // ignore: avoid_print
+    print("CERVEJA ${beer["name"]} ${beer["style"]} ${beer["ibu"]}");
+
+    return Scaffold(
+      appBar: AppBar(title: Text("")),
+      body: Center(
+        child: Text("${beer["name"]} ${beer["style"]} ${beer["ibu"]}"),
+      ),
+    );
   }
 }
