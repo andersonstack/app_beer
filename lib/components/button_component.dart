fe13abe1820f838b48b4dcc9083b160c2e22449b
andersonstack <andersong.pereiracruz@gmail.com>
2025-04-11 20:35:12 -0300

    refactor: criando fábrica de botões

--- lib/components/button_component.dart
+++ lib/components/button_component.dart
@@ -1,3 +1,4 @@
+import 'package:app_beer/style/btnStyle.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:app_beer/interface/icons_interface.dart';
@@ -10,19 +11,66 @@ class BtnView extends StatelessWidget {
   final String title;
   final String icon;
   final Function onPress;
+  final ButtonStyle? style;
 
-  const BtnView({
+  const BtnView._({
     super.key,
     required this.title,
     required this.icon,
     required this.onPress,
+    this.style,
   });
+
+  factory BtnView.deleteStyle({
+    Key? key,
+    required String title,
+    required VoidCallback onPress,
+    required String icon,
+  }) {
+    return BtnView._(
+      key: key,
+      title: title,
+      icon: icon,
+      onPress: onPress,
+      style: BtnStyle.deleteStyle,
+    );
+  }
+
+  factory BtnView.defaultStyle({
+    Key? key,
+    required String title,
+    required VoidCallback onPress,
+    required String icon,
+  }) {
+    return BtnView._(
+      key: key,
+      title: title,
+      icon: icon,
+      onPress: onPress,
+      style: BtnStyle.defaultStyle,
+    );
+  }
+
+  factory BtnView.editStyle({
+    Key? key,
+    required String title,
+    required VoidCallback onPress,
+    required String icon,
+  }) {
+    return BtnView._(
+      key: key,
+      title: title,
+      icon: icon,
+      onPress: onPress,
+      style: BtnStyle.editStyle,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton.icon(
       onPressed: () => onPress(),
-      style: ButtonStyle(),
+      style: style ?? BtnStyle.defaultStyle,
       label: Text(title),
       icon: icon == 'null' ? null : AppIcons.fromString(icon),
     );
