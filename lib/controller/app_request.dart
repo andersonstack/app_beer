aa8342d2cfd327e65826eb95ad7a66bca4a0170d
andersonstack <andersong.pereiracruz@gmail.com>
2025-04-17 07:33:01 -0300

    chore: removendo código morto

--- lib/controller/app_request.dart
+++ lib/controller/app_request.dart
@@ -1,21 +1,6 @@
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:get/get.dart';
 
 class AppRequest extends GetConnect {
-  // static Future<List<Map<String, dynamic>>> getBeers() async {
-  //   var beersUri = Uri(
-  //     scheme: 'https',
-  //     host: 'random-data-api.com',
-  //     path: 'api/beer/random_beer',
-  //     queryParameters: {'size': '5'},
-  //   );
-
-  //   var jsonString = await http.read(beersUri);
-  //   var beersJson = json.decode(jsonString) as List;
-  //   return beersJson.cast<Map<String, dynamic>>();
-  // }
-
   Future<List<Map<String, dynamic>>> getListBeers({int size = 5}) async {
     final response = await get(
       'https://random-data-api.com/api/beer/random_beer',
