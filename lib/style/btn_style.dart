efc956d90a5467ac5adff7a8809d864895135ad3
andersonstack <andersong.pereiracruz@gmail.com>
2025-04-17 10:38:53 -0300

    chore: removendo comentários

--- lib/style/btn_style.dart
+++ lib/style/btn_style.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 
 class BtnStyle {
-  // Estilo padrão (do seu tema global)
   static ButtonStyle get defaultStyle => ElevatedButton.styleFrom(
     backgroundColor: Colors.blue[700],
     foregroundColor: Colors.white,
   );
 
-  // Estilo para botão de deletar
   static ButtonStyle get deleteStyle => ElevatedButton.styleFrom(
     backgroundColor: Colors.red[700],
     foregroundColor: Colors.white,
   );
 
-  // Estilo para botão de editar
   static ButtonStyle get editStyle => ElevatedButton.styleFrom(
     backgroundColor: const Color(0xFFF2A20C),
     foregroundColor: Colors.white,
