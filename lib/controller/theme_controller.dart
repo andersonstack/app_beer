0ab48cfdbfeb073291ebd197cee3ab22a9240f04
andersonstack <andersong.pereiracruz@gmail.com>
2025-04-17 10:08:20 -0300

    feat: criando um controller para o tema

---
+++ lib/controller/theme_controller.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ThemeController extends GetxController {
+  var themeMode = ThemeMode.light.obs;
+
+  void toggleTheme() {
+    themeMode.value =
+        themeMode.value == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+  }
+
+  ThemeMode get currentTheme => themeMode.value;
+}
